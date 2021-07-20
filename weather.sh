1c454b0d8a563eeff40ca24eb5a03e850281545b
nieyuyao <nieyuyao0826@hotmail.com>
2021-07-20 21:01:35 +0800

    fix: add fake git config

--- weather.sh
+++ weather.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-curl -s 'wttr.in/{Beijing,Tianjin,Shijiazhuang,Tangshan}?format=%l:+%c+%t+%m+%w+%h\n' > ./README.md
+git config --local user.email "bot@github.com"
+git config --local user.name "bot"
+curl -s "wttr.in/{Beijing,Tianjin,Shijiazhuang,Tangshan}?format=%l:+%c+%t+%m+%w+%h\n" > ./README.md
 git add .
-git commit -m 'git action push'
+git commit -m 'push by git action'
 
