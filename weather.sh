dc32f543ea6456c9dc8c2b502a31419d3899bc55
nieyuyao <nieyuyao0826@hotmail.com>
2021-07-20 21:04:09 +0800

    add git push

--- weather.sh
+++ weather.sh
@@ -2,7 +2,9 @@
 
 git config --local user.email "bot@github.com"
 git config --local user.name "bot"
+git pull --rebase
 curl -s "wttr.in/{Beijing,Tianjin,Shijiazhuang,Tangshan}?format=%l:+%c+%t+%m+%w+%h\n" > ./README.md
 git add .
 git commit -m 'push by git action'
+git push -f
 
